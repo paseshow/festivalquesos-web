d3a64365633e87cf87f65145e51979f29d78362e
RogerBariles <rogerbariles@gmail.com>
2020-12-04 12:06:24 -0300

    implementacion del navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppRoutes } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar/navbar.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     RouterModule.forRoot(AppRoutes)
   ],
