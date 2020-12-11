bebf9b06b4c2c3e2cb53832fbe02de05c086b507
nairalucila <nairacoppari@gmail.com>
2020-12-11 10:58:46 -0300

    Validaciones del modal form correctas y hechas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
 
+// { //autentificacion './component/authentication/authentication.module'
+//         path: '',
+//         loadChildren: () => import('./component/pageInit/page-init.module').then(m => m.PageInitModule)
+//       },
+
 
 export const AppRoutes: Routes = [
   {
@@ -10,7 +15,7 @@ export const AppRoutes: Routes = [
         path: '',
         loadChildren: () => import('./component/pageInit/page-init.module').then(m => m.PageInitModule)
       },
-      {
+      {  
         path: 'autentificacion',
         loadChildren: () => import('./component/authentication/authentication.module').then(m => m.AuthenticationModule)
       },
