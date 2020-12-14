e1efe378576743a368e2363b899d15732aa3b4a2
RogerBariles <rogerbariles@gmail.com>
2020-12-14 11:52:56 -0300

    se bajo algunas imagenes por ahora

--- src/app/component/evento-stream/eventos-pasados/eventos-pasados.component.ts
+++ src/app/component/evento-stream/eventos-pasados/eventos-pasados.component.ts
@@ -46,9 +46,10 @@ export class EventosPasadosComponent implements OnInit {
         break;
 
       case "2017":
-        this.eventos = [1, 2, 3, 4, 5, 6, 7, 8, 9,
+        this.eventos = [1, 2, 3, 4, 5, 6];
+        /*7, 8, 9,
           10, 11, 12, 13, 14, 15, 16, 17, 18,
-          19, 20, 21, 22, 23, 24, 25, 26, 27];
+          19, 20, 21, 22, 23, 24, 25, 26, 27*/
         this.anioEventoAntes = "2016";
         this.anioEventoDespues = "2018";
         this.anioEvento = "2017";
