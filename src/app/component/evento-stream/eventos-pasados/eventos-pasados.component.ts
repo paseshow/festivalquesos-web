c2c3b9e70033281293e28394ea1e4e674e09ae74
RogerBariles <rogerbariles@gmail.com>
2020-12-14 12:12:46 -0300

    .

--- src/app/component/evento-stream/eventos-pasados/eventos-pasados.component.ts
+++ src/app/component/evento-stream/eventos-pasados/eventos-pasados.component.ts
@@ -46,7 +46,7 @@ export class EventosPasadosComponent implements OnInit {
         break;
 
       case "2017":
-        this.eventos = [1, 2, 3, 4, 5, 6];
+        this.eventos = [1, 2, 3, 4, 5];
         /*7, 8, 9,
           10, 11, 12, 13, 14, 15, 16, 17, 18,
           19, 20, 21, 22, 23, 24, 25, 26, 27*/
