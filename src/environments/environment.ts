5beb395eb60f5db327de96d40f3456a72ae41607
RogerBariles <rogerbariles@gmail.com>
2020-12-16 16:20:00 -0300

    correccion de footer y subida a web test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: "https://festivalquesos-api-test.herokuapp.com/",
- //apiUrl: "http://localhost:8080/"
+  apiUrl: "https://festival-api-prod.herokuapp.com/",
+  //apiUrl: "https://festivalquesos-api-test.herokuapp.com/",
+  //apiUrl: "http://localhost:8080/"
 };
 
 /*
