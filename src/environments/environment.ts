37db41a51c02686ebd37594e73f6ebfc7851cecb
nairalucila <nairacoppari@gmail.com>
2020-12-16 16:53:07 -0300

    agregados logos bialcohol y casalta

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "https://festivalquesos-api-test.herokuapp.com/",
+  apiUrl: "https://festival-api-prod.herokuapp.com/",
  //apiUrl: "http://localhost:8080/"
 };
 
